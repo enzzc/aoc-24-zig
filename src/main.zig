7eb1047b3dc17773a792561c95efe95c26bcf551
Lu Calamia <hello@ecalamia.com>
2024-12-17 19:20:29 +0100

    feat: aoc-1

---
+++ src/main.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const fmt = std.fmt;
+const input = @embedFile("inputs/aoc-1.txt");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var leftList = std.ArrayList(i32).init(allocator);
+    var rightList = std.ArrayList(i32).init(allocator);
+
+    var it = std.mem.tokenizeScalar(u8, input, '\n');
+    while (it.next()) |line| {
+        var parts = std.mem.tokenizeScalar(u8, line, ' ');
+        const leftPart = parts.next() orelse "";
+        const rightPart = parts.next() orelse "";
+        const left = fmt.parseInt(i32, leftPart, 10) catch @panic("Invalid input");
+        const right = fmt.parseInt(i32, rightPart, 10) catch @panic("Invalid input");
+
+        try leftList.append(left);
+        try rightList.append(right);
+    }
+
+    std.mem.sort(i32, leftList.items, {}, std.sort.asc(i32));
+    std.mem.sort(i32, rightList.items, {}, std.sort.asc(i32));
+
+    var acc: i32 = 0;
+    for (leftList.items, rightList.items) |l, r| {
+        var dist = l - r;
+        if (dist < 0) {
+            dist = -dist;
+        }
+        acc += dist;
+    }
+
+    std.debug.print("{}", .{acc});
+}
